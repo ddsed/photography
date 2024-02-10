54bd62b7ba2681c808ad78bb1a40c0621c6734d7
Dasha Sedova <dashassed@gmail.com>
2024-02-10 19:34:28 +0100

    feat: Khibiny

--- src/components/Place/Place.jsx
+++ src/components/Place/Place.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Header/Header';
+import place from '../../place.json';
 
 function Place() {
     return (
         <main className="place">
             <Header/>
-            <section className="place__container">
+            <section className="place__section">
                 <Link to="/search" className="place__back" rel="noreferrer">back</Link> 
+                <div className={`place__grid-container place__grid-container_${place.name}`}>
+                    {place.photos.map(photo => (
+                        <img 
+                            key={photo.id} 
+                            src={require(`../../images/khibiny/${photo.image}`)} 
+                            alt={place.name} 
+                            className={`${place.name}${photo.id}`} 
+                            style={{
+                                gridArea: `${place.name}${photo.id}`,
+                                objectFit: 'cover', 
+                                width: '100%', 
+                                height: '100%'
+                            }}
+                        />
+                    ))}
+                </div>
             </section>
         </main>
     );
