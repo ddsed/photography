ea41266bf51b526a4790017e0c025b4fd3e21871
Dasha Sedova <dashassed@gmail.com>
2024-02-16 02:55:07 +0100

    add: deploy 2.2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
