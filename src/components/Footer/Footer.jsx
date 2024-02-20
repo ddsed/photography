a6bf989eb546ec4d072822d82c019cec95dadd0a
Dasha Sedova <dashassed@gmail.com>
2024-02-20 09:26:30 +0100

    fix: code link open in a separate tab

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer className="footer">
-            <p className="footer__rights">&copy; 2024 <Link to="https://github.com/ddsed" className="footer__link">code</Link> and photos by Daria Sedova. All rights reserved.</p>
+            <p className="footer__rights">&copy; 2024 <Link to="https://github.com/ddsed" className="footer__link" rel="noreferrer" target="_blank">code</Link> and photos by Daria Sedova. All rights reserved.</p>
         </footer>   
     );
 }
